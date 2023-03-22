f0399ca1256251e355965ecfe41dafb168dd6422
Ali Kelkawi <ali.kelkawi@canonical.com>
2023-03-23 00:58:52 +0300

    CSS-3425: fix integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -31,7 +31,7 @@ async def deploy(ops_test: OpsTest):
     # Deploy temporal server, temporal admin and postgresql charms.
     await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME)
     await ops_test.model.deploy(APP_NAME_ADMIN, channel="beta")
-    await ops_test.model.deploy("postgresql-k8s", channel="edge")
+    await ops_test.model.deploy("postgresql-k8s", channel="edge", trust=True)
 
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(
