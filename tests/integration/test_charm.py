87623922c52070114f279074a7ebcc544bdaadbc
Ali Kelkawi <ali.kelkawi@canonical.com>
2023-03-22 21:50:11 +0300

    CSS-3425: add linting and deploy temporal-admin from charmhub

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,6 +2,8 @@
 # Copyright 2022 Canonical Ltd Ltd.
 # See LICENSE file for licensing details.
 
+"""Temporal charm integration tests."""
+
 import logging
 from multiprocessing import Process
 from pathlib import Path
@@ -17,9 +19,7 @@ logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
-
-METADATA_ADMIN = yaml.safe_load(Path("../temporal-admin-k8s-operator/metadata.yaml").read_text())
-APP_NAME_ADMIN = METADATA_ADMIN["name"]
+APP_NAME_ADMIN = "temporal-admin-k8s"
 
 
 @pytest_asyncio.fixture(name="deploy", scope="module")
@@ -28,12 +28,9 @@ async def deploy(ops_test: OpsTest):
     charm = await ops_test.build_charm(".")
     resources = {"temporal-server-image": METADATA["containers"]["temporal"]["upstream-source"]}
 
-    admin_resources = {"temporal-admin-image": METADATA_ADMIN["containers"]["temporal-admin"]["upstream-source"]}
-    admin_charm = await ops_test.build_charm("../temporal-admin-k8s-operator")
-
     # Deploy temporal server, temporal admin and postgresql charms.
     await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME)
-    await ops_test.model.deploy(admin_charm, resources=admin_resources, application_name=APP_NAME_ADMIN)
+    await ops_test.model.deploy(APP_NAME_ADMIN, channel="beta")
     await ops_test.model.deploy("postgresql-k8s", channel="edge")
 
     async with ops_test.fast_forward():
@@ -48,15 +45,8 @@ async def deploy(ops_test: OpsTest):
         await ops_test.model.integrate(f"{APP_NAME}:db", "postgresql-k8s:db")
         await ops_test.model.integrate(f"{APP_NAME}:visibility", "postgresql-k8s:db")
         await ops_test.model.integrate(f"{APP_NAME}:admin", f"{APP_NAME_ADMIN}:admin")
-        await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=False, timeout=600)
-
-        await ops_test.juju(
-            "exec",
-            "--unit",
-            "temporal-k8s/0",
-            "--",
-            "open-port",
-            "7233",
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME], status="active", raise_on_blocked=False, timeout=600
         )
 
         # Register default namespace from admin charm.
@@ -69,15 +59,19 @@ async def deploy(ops_test: OpsTest):
         logger.info(f"tctl result: {result}")
         assert "result" in result and result["result"] == "command succeeded"
 
-        await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", raise_on_blocked=False, timeout=600)
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME], status="active", raise_on_blocked=False, timeout=600
+        )
         assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
 
 @pytest.mark.abort_on_fail
 @pytest.mark.usefixtures("deploy")
 class TestDeployment:
-    async def test_basic_client(self, ops_test: OpsTest):
+    """Integration tests for Temporal charm."""
 
+    async def test_basic_client(self, ops_test: OpsTest):
+        """Connects a client and runs a basic Temporal workflow."""
         status = await ops_test.model.get_status()  # noqa: F821
         address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
         url = f"{address}:7233"
