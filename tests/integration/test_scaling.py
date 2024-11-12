43d3976a30c99ff12bcdc2ce93637b2a8b7889b2
Ali Kelkawi <ali.kelkawi@canonical.com>
2024-11-12 09:40:50 +0300

    increase number of connections

--- tests/integration/test_scaling.py
+++ tests/integration/test_scaling.py
@@ -46,7 +46,7 @@ async def deploy(ops_test: OpsTest):
     await ops_test.model.deploy(APP_NAME_ADMIN, channel="edge")
     await ops_test.model.deploy(APP_NAME_UI, channel="edge")
     await ops_test.model.deploy("postgresql-k8s", channel="14/stable", trust=True)
-    await ops_test.model.deploy("pgbouncer-k8s", channel="1/stable", trust=True, config={"max_db_connections": 200})
+    await ops_test.model.deploy("pgbouncer-k8s", channel="1/stable", trust=True, config={"max_db_connections": 300})
 
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(
