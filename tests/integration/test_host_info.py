398416518318817ffb2cdc651789572df179d6cc
Wyatt Rees <wyatt.rees@canonical.com>
2026-01-15 13:18:15 -0700

    fix build of test charm

--- tests/integration/test_host_info.py
+++ tests/integration/test_host_info.py
@@ -4,21 +4,12 @@
 """Temporal charm temporal-host-info relation integration tests."""
 
 import logging
-from pathlib import Path
 
 import jubilant
 import pytest
 from helpers import APP_NAME
 
 logger = logging.getLogger(__name__)
-
-
-@pytest.fixture(scope="module")
-def host_info_requirer(juju: jubilant.Juju) -> str | Path:
-    """Fetch the path to charm."""
-    charm = juju.build_charm("./tests/integration/host_info_requirer")
-    assert charm, "Charm not built"
-    return charm
 
 
 @pytest.fixture(scope="module")
@@ -33,7 +24,7 @@ def juju(deploy: str):
 class TestTemporalHostInfoRelation:
     """Tests for temporal-host-info relation."""
 
-    def test_relation(self, juju: jubilant.Juju, host_info_requirer: str | Path):
+    def test_relation(self, juju: jubilant.Juju):
         cfg = juju.config(APP_NAME)
         services = cfg["services"]["value"]
         new_cfg = {"external-hostname": "temporal.local.test"}
@@ -42,10 +33,7 @@ class TestTemporalHostInfoRelation:
             new_cfg["services"] = services
         juju.config(APP_NAME, new_cfg)
         # Deploy host info requirer charm
-        juju.deploy(
-            host_info_requirer,
-            application_name="host-info-requirer",
-        )
+        juju.deploy("./tests/integration/host_info_requirer", "host-info-requirer")
         juju.wait(jubilant.all_active, timeout=300)
         juju.integrate("host-info-requirer:temporal-host-info", f"{APP_NAME}:temporal-host-info")
         juju.wait(jubilant.all_active, timeout=300)
