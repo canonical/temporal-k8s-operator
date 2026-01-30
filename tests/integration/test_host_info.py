29614ae97fccbf948ebe12bb3ed6a79d88127b43
Wyatt Rees <wyatt.rees@canonical.com>
2026-01-30 11:30:56 -0700

    pack host-info-requirer charm directly in tox directive

--- tests/integration/test_host_info.py
+++ tests/integration/test_host_info.py
@@ -4,6 +4,7 @@
 """Temporal charm temporal-host-info relation integration tests."""
 
 import logging
+import pathlib
 
 import jubilant
 import pytest
@@ -19,12 +20,23 @@ def juju(deploy: str):
     return juju
 
 
+@pytest.fixture(scope="module")
+def host_info_requirer_charm() -> pathlib.Path:
+    """Return full absolute path to given test charm."""
+    name = "temporal-host-info-requirer"
+    charm_dir = pathlib.Path(__file__) / "host_info_requirer"
+    charms = [p.absolute() for p in charm_dir.glob(f"{name}_*.charm")]
+    assert charms, f"{name}_*.charm not found"
+    assert len(charms) == 1, "more than one .charm file, unsure which to use"
+    return charms[0]
+
+
 @pytest.mark.abort_on_fail
 @pytest.mark.usefixtures("deploy")
 class TestTemporalHostInfoRelation:
     """Tests for temporal-host-info relation."""
 
-    def test_relation(self, juju: jubilant.Juju):
+    def test_relation(self, juju: jubilant.Juju, host_info_requirer_charm: pathlib.Path):
         cfg = juju.config(APP_NAME)
         services = cfg["services"]
         new_cfg = {"external-hostname": "temporal.local.test"}
@@ -33,7 +45,7 @@ class TestTemporalHostInfoRelation:
             new_cfg["services"] = services
         juju.config(APP_NAME, new_cfg)
         # Deploy host info requirer charm
-        juju.deploy("./tests/integration/host_info_requirer", "host-info-requirer")
+        juju.deploy(host_info_requirer_charm, "host-info-requirer")
         juju.wait(jubilant.all_active, timeout=300)
         juju.integrate("host-info-requirer:temporal-host-info", f"{APP_NAME}:temporal-host-info")
         juju.wait(jubilant.all_active, timeout=300)
