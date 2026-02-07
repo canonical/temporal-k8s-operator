d54e8e5eae50cbc652bc0149174c1236fe8acac9
nikita-jare <nikitajare2021@gmail.com>
2026-02-07 14:56:14 -0500

    Fix: Added scenario.Exec mocks

--- tests/scenario/conftest.py
+++ tests/scenario/conftest.py
@@ -2,11 +2,13 @@
 # See LICENSE file for licensing details.
 
 import json
+import os
 
 import ops.testing
 import pytest
 
 from charm import TemporalK8SCharm
+from literals import LOG_OUTPUT_FILE
 
 
 def pytest_configure(config):  # noqa: DCO020
@@ -49,18 +51,30 @@ def context(temporal_k8s_charm):
 
 
 @pytest.fixture(scope="function")
-def temporal_container():
+def log_dir_execs():
+    log_dir = os.path.dirname(LOG_OUTPUT_FILE)
+    return {
+        ops.testing.Exec(["mkdir", "-p", log_dir]),
+        ops.testing.Exec(["chown", "-R", "ubuntu:ubuntu", log_dir]),
+        ops.testing.Exec(["logrotate", "/etc/logrotate.d/temporal-server"]),
+    }
+
+
+@pytest.fixture(scope="function")
+def temporal_container(log_dir_execs):
     return ops.testing.Container(
         "temporal",
         can_connect=True,
+        execs=log_dir_execs,
     )
 
 
 @pytest.fixture(scope="function")
-def temporal_container_initialized():
+def temporal_container_initialized(log_dir_execs):
     return ops.testing.Container(
         "temporal",
         can_connect=True,
+        execs=log_dir_execs,
         check_infos=[ops.testing.CheckInfo("temporal-server-running")],
         layers={
             "initialized-layer": ops.pebble.Layer(
@@ -95,10 +109,11 @@ def incomplete_layer_dict():
 
 
 @pytest.fixture(scope="function")
-def temporal_container_incomplete_layer(incomplete_layer_dict):
+def temporal_container_incomplete_layer(incomplete_layer_dict, log_dir_execs):
     return ops.testing.Container(
         "temporal",
         can_connect=True,
+        execs=log_dir_execs,
         layers={
             "incomplete-layer": ops.pebble.Layer(incomplete_layer_dict),
         },
