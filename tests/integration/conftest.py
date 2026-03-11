1cc468279346d0b010ff8ac50c6128c17a2898eb
nikita-jare <nikitajare2021@gmail.com>
2026-03-11 18:18:48 -0400

    fix(ci): resolve integration test failures

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -65,7 +65,9 @@ async def deploy(ops_test: OpsTest, charm: str):
             apps=["postgresql-k8s", "self-signed-certificates"], status="active", raise_on_blocked=False, timeout=1200
         )
 
-        await ops_test.model.integrate("self-signed-certificates", "postgresql-k8s")
+        await ops_test.model.integrate(
+            "postgresql-k8s:certificates", "self-signed-certificates:certificates"
+        )
         await ops_test.model.wait_for_idle(
             apps=["postgresql-k8s", "self-signed-certificates"], status="active", raise_on_blocked=False, timeout=1200
         )
